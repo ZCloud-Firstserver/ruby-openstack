e8d0f1b353994cd40d4095d5af063c0ea957ca54
azukiwasher <uchiyamano@firstserver.co.jp>
2017-05-10 06:23:42 +0000

    require OpenStack::Network::FloatingIp

--- lib/openstack.rb
+++ lib/openstack.rb
@@ -50,6 +50,7 @@ module OpenStack
   require 'openstack/network/subnet'
   require 'openstack/network/router'
   require 'openstack/network/port'
+  require 'openstack/network/floating_ip'
   require 'openstack/version'
   # Constants that set limits on server creation
   MAX_PERSONALITY_ITEMS = 5
